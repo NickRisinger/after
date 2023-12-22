8e9bcb38f4cbcfa2ab0a5349950357cea4e675b3
Boozy044 <boozy044@gmail.com>
2023-12-22 16:03:16 +0300

    chore: update pages structure

--- src/app/lk/layout.tsx
+++ src/app/lk/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
-import { Manrope } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-const manrope = Manrope({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
-      <body style={manrope.style}>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
